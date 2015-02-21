749760eb6595837bdfc67da6a783ef07d9872a43
mackerbruny <michael.bruny.groth@gmail.com>
2015-02-21 17:46:46 -0600

    Problem while holding x and clicking only

--- js/app.js
+++ js/app.js
@@ -1,34 +1,59 @@
-$(document).ready(function() {
-	$(".ryu").mouseenter(function() {
-		$(".ryu-still").hide();
-		$(".ryu-ready").show();
+$(document).ready(function(){
+    $('.ryu').mouseenter(function(){
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();
+    })
+    .mouseleave(function() {
+        $('.ryu-still').show();
+        $('.ryu-ready').hide();
+    })
+    .mousedown(function(){
+        playHadouken();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate(
+            {'left': '1020px'},
+            500,
+            function(){
+                $(this).hide();
+                $(this).css('left', '659px');
+            });
+    })
+    .mouseup(function(){
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+    })
+    $(document).keydown(function(e) {
+	    if (e.keyCode == 88) { 
+	    	$('.ryu-ready').hide();
+	    	$('.ryu-still').hide();
+	    	$('.ryu-throwing').hide();
+			$('.ryu-cool').show();
+		}
 	})
-	.mouseleave(function() {
-		$(".ryu-ready").hide();
-		$(".ryu-still").show();
-	})
-	.mousedown(function() {
-		playHadouken();
-		$(".ryu-ready").hide();
-		$(".ryu-throwing").show();
-		$('.hadouken').finish().show()
-		.animate(
-			{'left': '1020px'},
-		  	500,
-		  	function() {
-		  		$(this).hide();
-		    	$(this).css('left', '635px');
-		  	}
-		);
-	})
-	.mouseup(function() {
-		$(".ryu-throwing").hide();
-		$(".ryu-ready").show();
-	})
+	.keyup(function(e) {
+	    if (e.keyCode == 88) {
+			$('.ryu-cool').hide();
+			$('.ryu-still').show();
+		}
+    });
 });
 
-function playHadouken () {
-	$('#hadouken-sound')[0].volume = 0.5;
-	$('#hadouken-sound')[0].load();
-	$('#hadouken-sound')[0].play();
-}
+function playHadouken() {
+        $('#hadouken-sound')[0].volume = 0.5;
+        $('#hadouken-sound')[0].load();
+        $('#hadouken-sound')[0].play();
+    }
+
+	/*$("html").keydown(function(e) {
+	    if (e.keyCode == 88) { 
+			$(".ryu-still").hide();
+			$(".ryu-ready").hide();
+			$(".ryu-cool").show();
+		}
+	})
+	.keyup(function(e) {
+	    if (e.keyCode == 88) {
+			$(".ryu-cool").hide();
+			$(".ryu-still").show();
+		}*/
